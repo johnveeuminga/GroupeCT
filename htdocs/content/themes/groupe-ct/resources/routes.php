43fbe117b4b2eb60b164c476b24963342cd6531d
John Vee Uminga <johnveeuminga@gmail.com>
2018-06-14 22:18:01 +0800

    URL fixing.

--- htdocs/content/themes/groupe-ct/resources/routes.php
+++ htdocs/content/themes/groupe-ct/resources/routes.php
@@ -12,7 +12,7 @@ Route::any('front', 'PageController@index');
 Route::get('singular', ['job_offer', 'uses' => 'LightboxController@job_offer']);
 Route::get('singular', ['product', 'uses' => 'ProductController@single']);
 Route::get('product-brands', 'ProductController@getProductBrands');
-Route::get('product-category', 'ProductController@productSubCat');
+Route::get('product-categories', 'ProductController@productSubCat');
 Route::get('tax', ['product_brands', 'uses' => 'ProductController@getProductsWithBrandType']);
 Route::any('tax', ['product_cat', 'uses' => 'ProductController@getProductsWithBrandType']);
 
